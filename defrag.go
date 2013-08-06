5944457a585340debfa970dc30d5fdd8b8a93c3a
Daniel Farina <daniel@fdr.io>
2013-08-05 18:44:13 -0700

    Document the defragmentation mechanism.
    Also use slightly less flow control in the WriteTo method to achieve a
    similar end, at the cost of more indentation, but I think this
    communicates the intent more crisply.

--- defrag.go
+++ defrag.go
@@ -10,6 +10,17 @@ type fragment struct {
 	contents io.ReadCloser
 }
 
+// The Defragmenter is an implementer of the WriterTo interface that
+// operates on the principle of handing out fragments, each with an
+// ordinal, expecting to receive them once they've been augmented with
+// a io.ReadCloser via 'register'.
+//
+// The registration step can happen out-of-order.
+//
+// Upon each registration, defrag.WriteTo will determine whether the
+// fragment can start being emitted right away or if it must be stored
+// until later work is completed to allow the bytes to be written in
+// the proper order.
 type defrag struct {
 	// The last successfully written fragment ordinal.
 	lastWritten int64
@@ -24,8 +35,9 @@ type defrag struct {
 	// Fragments being held for future defragmentation.
 	future map[int64]*fragment
 
-	// Accepts new fragments ready to be defragmented.
-	completeNotify chan *fragment
+	// Accepts new fragments that are pending defragmentation and
+	// emission.
+	registerNotify chan *fragment
 
 	// Injects an error to be emitted from WriteTo.
 	//
@@ -47,11 +59,13 @@ func newDefrag() *defrag {
 
 func (d *defrag) initDefrag() {
 	d.future = make(map[int64]*fragment)
-	d.completeNotify = make(chan *fragment)
+	d.registerNotify = make(chan *fragment)
 	d.cancelNotify = make(chan error)
 	d.lastOrdinalNotify = make(chan int64)
 }
 
+// Generate a new fragment.  These are numbered in the order they
+// appear in the output stream.
 func (d *defrag) nextFragment() *fragment {
 	atomic.AddInt64(&d.lastAlloc, 1)
 	f := fragment{ord: d.lastAlloc}
@@ -59,10 +73,17 @@ func (d *defrag) nextFragment() *fragment {
 	return &f
 }
 
+// Inject an error that will surface from defrag.WriteTo, effectively
+// canceling the write.
+//
+// This blocks until it is known that the cancellation has been
+// injected, and no more Write calls are thought to be made from
+// defrag.WriteTo.
 func (d *defrag) cancel(err error) {
 	d.cancelNotify <- err
 }
 
+// Write the contents of the defragmenter out to the io.Writer dst.
 func (d *defrag) WriteTo(dst io.Writer) (written int64, err error) {
 	// Early exit if previously canceled.
 	if d.cancellation != nil {
@@ -79,56 +100,68 @@ func (d *defrag) WriteTo(dst io.Writer) (written int64, err error) {
 			break
 		}
 
-		// Get fragment, but also check for cancellation.
-		var frag *fragment
 		select {
-		case frag = <-d.completeNotify:
-			break
+		case frag := <-d.registerNotify:
+			// Got fragment.
+			//
+			// Figure out whether to write it now or not.
+			next := d.lastWritten + 1
+			if frag.ord == next {
+				// This fragment completes the next
+				// contiguous swathe of bytes to be
+				// written out.
+				n, err := d.writeConsecutive(dst, frag)
+				d.written += n
+				if err != nil {
+					return d.written, err
+				}
+			} else if frag.ord > next {
+				// Got a fragment that can't be
+				// emitted yet: store it for now.
+				d.future[frag.ord] = frag
+			} else {
+				return d.written, assertErrf(
+					"Unexpected retrograde fragment %v, "+
+						"expected at least %v",
+					frag.ord, next)
+			}
+
 		case d.cancellation = <-d.cancelNotify:
+			// Cancel and exit immediately with the
+			// injected cancellation.
 			d.future = nil
 			return d.written, d.cancellation
-		case d.lastOrdinal = <-d.lastOrdinalNotify:
-			continue
-		}
 
-		next := d.lastWritten + 1
-		if frag.ord == next {
-			// This fragment completes the next contiguous
-			// swathe of bytes to be written out.
-			n, err := d.writeConsecutive(dst, frag)
-			d.written += n
-			if err != nil {
-				return d.written, err
-			}
-		} else if frag.ord > next {
-			// Got a fragment that can't be emitted yet:
-			// store it for now.
-			d.future[frag.ord] = frag
-		} else {
-			return d.written, assertErrf(
-				"Unexpected retrograde fragment %v, "+
-					"expected at least %v", frag.ord, next)
+		case d.lastOrdinal = <-d.lastOrdinalNotify:
+			// Re-check the exit conditions as a
+			// lastOrdinal has been set.
+			continue
 		}
 	}
 
 	return d.written, nil
 }
 
+// Set the last work ordinal to be processed once this is known, which
+// can allow defrag.WriteTo to terminate.
 func (d *defrag) setLast(lastOrdinal int64) {
 	d.lastOrdinalNotify <- lastOrdinal
 }
 
+// Get the last allocated fragment.
 func (d *defrag) lastAllocated() int64 {
 	return atomic.LoadInt64(&d.lastAlloc)
 }
 
+// Register a new fragment for defragmentation.
 func (d *defrag) register(frag *fragment) {
-	d.completeNotify <- frag
+	d.registerNotify <- frag
 }
 
+// Write a contiguous swathe of fragments, starting at the work
+// ordinal in 'start'.
 func (d *defrag) writeConsecutive(dst io.Writer, start *fragment) (
 	int64, error) {
-
 	// Write out the explicitly passed fragment.
 	written, err := io.Copy(dst, start.contents)
 	defer start.contents.Close()
@@ -139,7 +172,8 @@ func (d *defrag) writeConsecutive(dst io.Writer, start *fragment) (
 
 	// Write as many contiguous bytes as possible.
 	for {
-		// Check for sent cancellation.
+		// Check for sent cancellation between each fragment
+		// to abort earlier, when possible.
 		select {
 		case d.cancellation = <-d.cancelNotify:
 			d.future = nil
